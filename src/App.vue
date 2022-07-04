69dd9400a8529e099d7d3984b6e9df6f5944edec
Yaoshuyi1999 <yaoshuyi19990118@163.com>
2022-07-04 20:33:20 +0800

    11

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,32 @@
 <template>
-  <div>qq</div>
+  <div>
+    <bookMan></bookMan>
+  </div>
 </template>
 
 <script>
+import bookMan from './components/bookMan.vue'
 export default {
-
+  components: {
+    bookMan,
+  },
+  data () {
+    return {
+      bookObj:{
+        id:'',
+        bookname:'',
+        author:'',
+        publisher:'',
+      }
+    }
+  },
+  created () {
+    this.$axios({
+      url: '/api/getbooks'
+    }).then((res)=>{
+        console.log(res.data);
+    })
+  },
 }
 </script>
 
---
+++ src/components/bookMan.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <input type="search" placeholder="搜索-书本名称" class="search">
+    <table>
+      <thead>
+        <th>序号</th>
+        <th>书名</th>
+        <th>作者</th>
+        <th>出版商</th>
+        <th>操作</th>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+
+export default {
+  methods: {
+  }
+
+}
+</script>
+
+<style>
+  .search{
+    font-size: 15px;
+    padding: 5px 0;
+    padding-left: 10px;
+  }
+  table{
+    margin-top: 10px;
+    width: 800px;
+    border-collapse: collapse;
+    text-align: left;
+  }
+  th,
+  td{
+    border: 1px solid #ccc;
+    height: 30px;
+    padding-left: 5px;
+  }
+  thead{
+    border-bottom: 2px solid black;
+  }
+</style>
