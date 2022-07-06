53de21a0f0836411387f705a89e182de298c24a9
Yaoshuyi1999 <yaoshuyi19990118@163.com>
2022-07-06 09:14:32 +0800

    项目的初始化搭建完成

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <template>
-  <div>qq</div>
+  <div>
+    <MyHeader></MyHeader>
+    <div style="margin-top:45px">
+      <MyGoods></MyGoods>
+    </div>
+    <MyFooter></MyFooter>
+  </div>
 </template>
 
 <script>
+import MyHeader from './components/MyHeader'
+import MyGoods from './components/MyGoods'
+import MyFooter from './components/MyFooter'
 export default {
-
+  components: {
+    MyHeader,
+    MyGoods,
+    MyFooter
+  }
 }
 </script>
 
