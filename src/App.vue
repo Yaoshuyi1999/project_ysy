af2632055272c0e1278862f3b7bb4674d8bedc0a
Yaoshuyi1999 <yaoshuyi19990118@163.com>
2022-07-07 17:06:58 +0800

    知识点结束

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,57 @@
 <template>
-  <div>qq</div>
+  <div>
+    <MyHeader title="tabbar"></MyHeader>
+    <div style="margin-top:45px">
+      <component :is="comName"></component>
+    </div>
+    <MyTabBar :list='tabList' @changeTab="changeTabFn"></MyTabBar>
+  </div>
 </template>
 
 <script>
-export default {
+import MyHeader from "./components/MyHeader"
+import MyTabBar from "./components/MyTabBar"
+// import MyTable from "./components/MyTable"
+import MyGoodsList from './views/MyGoodsList'
+import MyGoodsSearch from './views/MyGoodsSearch'
+import MyUserInfo from './views/MyUserInfo'
 
+export default {
+  components: {
+    MyHeader,
+    MyTabBar,
+
+    MyGoodsList,
+    MyGoodsSearch,
+    MyUserInfo,
+  },
+  data () {
+    return {
+      comName:'MyGoodsList',
+      tabList: [
+        {
+            iconText: "icon-shangpinliebiao",
+            text: "商品列表",
+            componentName: "MyGoodsList"
+        },
+        {
+            iconText: "icon-sousuo",
+            text: "商品搜索",
+            componentName: "MyGoodsSearch"
+        },
+        {
+            iconText: "icon-user",
+            text: "我的信息",
+            componentName: "MyUserInfo"
+        }
+      ]
+    }
+  },
+  methods: {
+    changeTabFn(val){
+      this.comName=val
+    }
+  }
 }
 </script>
 
---
+++ src/components/MyTable.vue
@@ -0,0 +1,25 @@
+<template>
+  <table class="table table-bordered table-stripped">
+    <!-- 表格标题区域 -->
+    <thead>
+      <tr>
+        <slot name="header"></slot>
+      </tr>
+    </thead>
+    <!-- 表格主体区域 -->
+    <tbody>
+      <tr v-for="obj in arr" :key="obj.id">
+        <slot name="tbody" :row="obj"></slot>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "MyTable",
+  props: {
+    arr: Array,
+  },
+};
+</script>
