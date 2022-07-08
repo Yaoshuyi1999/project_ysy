5f145ccc9045de5c55fbdbc793180a11286eb09f
Yaoshuyi1999 <yaoshuyi19990118@163.com>
2022-07-08 18:19:49 +0800

    完成了基本内容

--- src/main.js
+++ src/main.js
@@ -6,6 +6,16 @@ import "./assets/fonts/iconfont.css"
 
 Vue.config.productionTip = false
 
+import axios from "axios";
+axios.defaults.baseURL = "https://www.escook.cn";
+Vue.prototype.$axios=axios
+
+Vue.directive("focus", {
+  inserted(el){
+    el.focus()
+  }
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
